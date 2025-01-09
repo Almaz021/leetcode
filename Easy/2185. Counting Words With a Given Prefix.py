9f18cee30f65cb528960a0efa858cd33972e5700
Almaz <almaz.kzn2005@yandex.ru>
2025-01-09 12:59:37 +0300

    problem 2185

---
+++ Easy/2185. Counting Words With a Given Prefix.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def prefixCount(self, words: List[str], pref: str) -> int:
+        ans = 0
+
+        for i in words:
+            if i.startswith(pref):
+                ans += 1
+
+        return ans
