9f433cc119e04e7cd5d75ddc04c8fc5c7d2c8103
Almaz <almaz.kzn2005@yandex.ru>
2025-02-21 16:04:46 +0300

    problem 125

--- Easy/125. Valid Palindrome.py
+++ Easy/125. Valid Palindrome.py
@@ -1,6 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        a = "abcdefghijklmnopqrstuvwxyz"
+        a = "abcdefghijklmnopqrstuvwxyz0123456789"
         res = ''
         for i in s:
             if i.lower() in a:
